e8074949cd7cf0413533a9a08c8ef81840a3d9c3
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-26 14:12:29 +0200

    rating component created

---
+++ src/components/Rating/index.js
@@ -0,0 +1,13 @@
+import { Star, Wrapper } from "./styled";
+
+export const Rating = () => {
+  return (
+    <>
+      <Wrapper>
+        <Star />
+        7,8 / 10
+      </Wrapper>
+      350 votes
+    </>
+  )
+};
