2cad73233715648fc9fce64eae95afa88ba0af43
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-26 14:13:15 +0200

    moviePage structure initiated, examples added

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,7 +1,43 @@
+import { Container } from "../../../components/Container";
+import { Header } from "../../../components/Header";
+import { Rating } from "../../../components/Rating";
+import { HeaderWrapper } from "./styled";
+import { Section } from "../../../components/Section/index";
+import { ActorsPage } from "../../actors/ActorsPage";
+import { MovieTile } from "../MovieTile";
+
 export const MoviePage = () => {
+  const task = true;
+
+  if (!task) {
+    return (
+      <Container>
+        <div>Header z obrazkiem na full</div>
+        <div>Header z obrazkiem na full</div>
+        <div>Header z obrazkiem na full</div>
+      </Container>
+    );
+  };
+
   return (
-    <>
-      <div>Movie1</div>
-    </>
+    <Container>
+      <HeaderWrapper>
+        <Header title="Mulan long title" />
+        <Rating />
+      </HeaderWrapper>
+
+      <Section
+        body={<MovieTile />} />
+
+      <Section
+        title="Cast"
+        body={<ActorsPage />}
+      />
+
+      <Section
+        title="Crew"
+        body={<ActorsPage />}
+      />
+    </Container>
   );
-}
+};
