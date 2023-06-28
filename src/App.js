4fb6883c0c5b06e8db92a004561bcff7fac1f11e
Adrian Bogdan <enu999@o2.pl>
2023-06-28 18:46:18 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navigation from "./common/Navigation/index";
 import { ActorsPage } from "./features/actors/ActorsPage";
 import { MoviePage } from "./features/movies/MoviePage/index";
+import { MoviesPage } from "./features/movies/MoviesPage";
+import { MoviesList } from "./features/movies/MoviesPage/MoviesList";
 
 export const App = () => {
   return (
     <>
       <Navigation />
-      <ActorsPage />
+      <MoviesList />
     </>
   );
 };
