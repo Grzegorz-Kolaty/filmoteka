0f81f03250f0063c3c97c2442377edbd26896eb6
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-06-29 21:01:55 +0200

    route

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { MoviePage } from "./features/movies/MoviePage/index";
 import { MoviesPage } from "./features/movies/MoviesPage/index";
 import { ActorPage } from "./features/actors/ActorPage/index";
 import { Container } from "./components/Container";
-import { HashRouter, Routes } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 export const App = () => {
   return (
     <HashRouter>
       <Navigation />
       <Container>
-        <Routes exact path="/actors" component={ActorsPage} />
-        <Routes exact path="/actor" component={ActorPage} />
-        <Routes exact path="/movie" component={MoviePage} />
-        <Routes exact path="/movies" component={MoviesPage} />
+        <Route exact path="/actors" component={ActorsPage} />
+        <Route exact path="/actor" component={ActorPage} />
+        <Route exact path="/movie" component={MoviePage} />
+        <Route exact path="/movies" component={MoviesPage} />
       </Container>
     </HashRouter>
   );
