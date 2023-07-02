5410717552ba0954022881cbfaf8960bd89ac0d9
Przemek8686 <1986krawiec1986@gmail.com>
2023-07-02 20:43:32 +0200

    to  MovieTile Api added

--- src/features/movies/MovieTile/index.js
+++ src/features/movies/MovieTile/index.js
@@ -1,19 +1,47 @@
-import { Rating } from "../../../components/Rating";
-import { Tile } from "../../../components/Tile";
-import poster from "../../../components/images/poster.png";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = "991805bb8d078db21dd78fe533903f2b";
+const API_URL = "https://api.themoviedb.org/3/movie/";
 
 export const MovieTile = () => {
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetchMovieTile();
+  }, []);
+
+  const fetchMovieTile = async () => {
+    try {
+      const movieId = 123; // Replace with the desired movie ID
+      const response = await axios.get(
+        `${API_URL}${movieId}?api_key=${API_KEY}`
+      );
+      setMovie(response.data);
+    } catch (error) {
+      console.log("An error occurred while fetching the data:", error);
+    }
+  };
+
+  if (!movie) {
+    return <div>Loading data...</div>;
+  }
+
   return (
-    <Tile
-      img={poster}
-      title="Mulan"
-      year="2020"
-      subtitle="Production: "
-      subtitleText="China, United States"
-      subtitle2="Release date: "
-      subtitleText2="24.10.2020"
-      description="A young Chinese maiden disguises herself as a male warrior in order to save her father.
-          Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father."
-    />
+    
+      <div>
+        <article>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+          <div>
+            <header>{movie.title}</header>
+            <span>Release Date: {movie.release_date}</span>
+            <br />
+            <span>Rating: {movie.vote_average}</span>
+            <p>{movie.overview}</p>
+          </div>
+        </article>
+      </div>
+    
   );
 };
