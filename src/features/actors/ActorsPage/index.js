4ad4532d117c283a2ab957a360e857e40a88c003
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-07-03 19:42:52 +0200

    homepage new link

--- src/features/actors/ActorsPage/index.js
+++ src/features/actors/ActorsPage/index.js
@@ -1,7 +1,9 @@
+// import { Container } from "../../../components/Container";
+// import { Section } from "../../../components/Section";
+// // import { ActorsList } from "./ActorsList";
+// import ActorList from "./getActorsList";
+
 import { Container } from "../../../components/Container";
-import { Section } from "../../../components/Section";
-// import { ActorsList } from "./ActorsList";
-import ActorList from "./getActorsList";
 
 export const ActorsPage = () => {
   return (
