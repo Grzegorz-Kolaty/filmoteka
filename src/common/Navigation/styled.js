6391f566a489e1df8972e6e75bbe0769bed99319
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-07-05 20:09:13 +0200

    navigation and search stylization

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,117 +1,83 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const Nav = styled.header`
+export const Nav = styled.nav`
   background: ${({ theme }) => theme.color.black};
+  padding: 23px;
+
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  margin: 0 auto;
+  max-width: 1368px;
+
+  @media (max-width: 900px) {
+    grid-template-columns: auto;
+    grid-gap: 24px;
+  }
+
 `;
 
 export const NavContainer = styled.div`
-  margin: auto;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  max-width: 1330px;
-  padding: 0 16px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-    justify-content: space-between;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    padding: 8px;
-  }
+  display: grid;
+  grid-template-columns: auto auto;
 `;
 
 export const Logo = styled.div`
-  display: inherit;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
   align-items: center;
-  margin-right: 75px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-    margin-right: 24px;
-    margin-top: 4px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    margin-right: 8px;
-  }
-`;
+  gap: 20px;
+ `;
 
 export const Icon = styled.img`
   width: 40px;
   height: 40px;
-  margin-top: 4px;
-
-  margin: 0 12px 0 0;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    width: 17px;
-    height: 17px;
-  }
 `;
 
 export const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 0;
   font-weight: 500;
-  line-height: 40px;
   font-size: 24px;
   text-transform: capitalize;
   letter-spacing: -0.5px;
   color: ${({ theme }) => theme.color.white};
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-    font-size: 20px;
-    line-height: 1.3;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    font-size: 13px;
-    line-height: 1.3;
-  }
 `;
 
 export const List = styled.ul`
-  color: white;
   display: flex;
-  flex-wrap: wrap;
-  list-style: none;
+  flex-flow: row nowrap;
+  justify-content: center;
   align-items: center;
+  gap: 15px;
+  color: white;
   height: 50px;
-  margin-right: 20px;
   padding: 0;
+  margin: 0;
 `;
 
 export const NavigationLink = styled(Link)`
-  display: inherit;
-  padding: 8px 24px;
-  margin: 23px 0 24px 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-radius: 24px;
   color: ${({ theme }) => theme.color.white};
   border: 1px solid transparent;
   text-transform: uppercase;
   font-weight: 600;
   font-size: 14px;
-  line-height: 21px;
   cursor: pointer;
   text-decoration: none;
-  margin-top: 8px;
+  padding: 10px;
+  border: 2px solid transparent;
 
-  &:hover,
-  &.active {
-    border: 1px solid ${({ theme }) => theme.color.white};
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-    border-radius: 30px;
-    font-size: 13px;
-    line-height: 19px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    border-radius: 29px;
-    font-size: 12px;
-    line-height: 18px;
-    padding: 8px;
-    margin: 10px 0;
+  &:hover {
+    border: 2px solid ${({ theme }) => theme.color.white};
   }
 `; 
--- src/features/movies/MoviesPage/Search/styled.js
+++ src/features/movies/MoviesPage/Search/styled.js
@@ -2,62 +2,25 @@ import styled from "styled-components";
 import { ReactComponent as Search } from "../../../../components/images/searchIcon.svg";
 
 export const SearchWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: ${({ theme }) => theme.color.white};
     border: 1px solid ${({ theme }) => theme.color.grey};
     border-radius: 33px;
-    display: flex;
-    align-items: center;
-    max-width: 432px;
+    padding: 15px;
+    gap: 15px;
     width: 100%;
-    margin-bottom:5px;
-    margin-left:180px;
-   
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-        max-width: 300px;
-    };
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 13px;
-        line-height: 16px;
-        margin: auto;
-        
-    };
 `;
 
 export const Input = styled.input`
     border: none;
-    width: 100%;
     font-size: 16px;
-    font-weight: 400;
-    line-height: 1.5;
     background-color: transparent;
     outline: 0;
-    padding: 0;
-  
-
-    &::placeholder {
-        color: ${({ theme }) => theme.color.darkGrey};
-    }
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-        font-size: 15px
-    };
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 13px;
-    };
+    padding: 0px;
 `;
 
 export const SearchIcon = styled(Search)`
-    margin: 12px;
-    cursor:pointer;
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.smallScreen}px) {
-        width: 36px;
-    };
-
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        width: 16px;
-    };
+    cursor: pointer;
 `;
