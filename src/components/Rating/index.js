379ee4c07b3af7f800cf6e23544f320f59bee0d4
Przemek8686 <1986krawiec1986@gmail.com>
2023-07-18 19:31:22 +0200

    Merge branch 'main' into PrzemekIMG

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -15,4 +15,4 @@ export const Rating = ({ rating, votes, header }) => {
       )}
     </Wrapper>
   );
-};
+}
--- src/features/movies/MoviesPage/Search/index.js
+++ src/features/movies/MoviesPage/Search/index.js
@@ -1,12 +1,33 @@
 import React from "react";
-
 import { SearchWrapper, Input, SearchIcon } from "./styled";
+import searchQueryParamName from "../../../searchQueryParamName";
+import {
+  useQueryParameter,
+  useReplaceQueryParameter,
+} from "../../../queryParameters";
+
 const Search = () => {
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
+
+  const onInputChange = ({ target }) => {
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    });
+  };
+
   return (
     <SearchWrapper>
       <SearchIcon />
-      <Input placeholder="Search for movies..." type="text" />
+      <Input
+        placeholder="Search for movies..."
+        type="text"
+        value={query || ""}
+        onChange={onInputChange}
+      />
     </SearchWrapper>
   );
 };
+
 export default Search;
