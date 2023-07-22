dfd87d6efcbb0a3ec5146da44a4bc0df298082a9
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-07-22 11:19:53 +0200

    refreshing website to movies fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { Container } from "./components/Container";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import Navigation from "./common/Navigation/index";
 import { ActorsPage } from "./features/actors/ActorsPage/index";
 import { MoviePage } from "./features/movies/MoviePage/index";
 import { MoviesPage } from "./features/movies/MoviesPage/index";
 import { ActorPage } from "./features/actors/ActorPage/index";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import { toActors, toMovies } from "./routes";
 
 export const App = () => {
@@ -12,23 +11,21 @@ export const App = () => {
     <HashRouter>
       <Navigation />
       <Switch>
-        <Container>
-          <Route path={"/actor"}>
-            <ActorPage />
-          </Route>
-          <Route path={"/movie"}>
-            <MoviePage />
-          </Route>
-          <Route path={toActors()}>
-            <ActorsPage />
-          </Route>
-          <Route path={toMovies()}>
-            <MoviesPage />
-          </Route>
-          <Route path="/">
-            <Redirect to={toMovies()} />
-          </Route>
-        </Container>
+        <Route path="/actor">
+          <ActorPage />
+        </Route>
+        <Route path="/movie">
+          <MoviePage />
+        </Route>
+        <Route path={toActors()}>
+          <ActorsPage />
+        </Route>
+        <Route path={toMovies()}>
+          <MoviesPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/movies" />
+        </Route>
       </Switch>
     </HashRouter>
   );
