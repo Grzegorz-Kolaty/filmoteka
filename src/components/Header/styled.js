ba6b51786521d66828257568e46fa530e94755df
Przemek8686 <1986krawiec1986@gmail.com>
2023-07-27 19:44:22 +0200

    correction correction styled MoviePage and Header

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,11 +1,21 @@
 import styled from "styled-components";
 
 export const Headline = styled.h1`
-  width: 503px;
-  height: 77px;
   font-size: 64px;
   font-family: Poppins;
   font-weight: 600;
-  line-height: 120%;
+  line-height: 14%;
   gap: 24px;
+  margin-left: 26px;
+
+  @media (max-width:450px) {
+    font-size: 24px;
+    line-height: 110%;
+    display: flex;
+    align-items: center;
+    width: 64px;
+    position: absolute;
+    top: 50.67%;
+    margin-left:29px;
+  }
 `;
